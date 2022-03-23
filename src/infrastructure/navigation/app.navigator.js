7a689cb3531ddd167335480aafdb67b721da139b
Cole Fortner <colefortner@gmail.com>
2022-03-23 13:15:24 -0400

    Installed @react-navigation/bottom-tabs and switched navigation set up to the bottom tab navigator

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const HomeScreen = () => {
   return (
@@ -11,14 +11,30 @@ const HomeScreen = () => {
   );
 };
 
-const Stack = createNativeStackNavigator();
+const AnotherScreen = () => {
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Another Screen</Text>
+  </View>;
+};
+
+const SettingsScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Setting Screen</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Another Screen" component={AnotherScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
