fc99cc1376d60eba7dfe83ab75059c2e3a918f43
Cole Fortner <colefortner@gmail.com>
2022-03-23 23:28:55 -0400

    Install react-native-vector-icons and use paw icon to create a rating View that displays the number of paws associated with the business rating

--- src/components/features/businesses/components/business-info-card.component.js
+++ src/components/features/businesses/components/business-info-card.component.js
@@ -2,19 +2,35 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {Card} from 'react-native-paper';
 import styled from 'styled-components/native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const BusinessCard = ({business = {}}) => {
   const {
     name = 'Pinellas Ale House',
     image = 'https://www.pawbeer.com/wp-content/uploads/Webpage-Header-1024x666.jpg',
     address = '1962 1st Ave S.',
+    rating = 3,
   } = business;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  Ionicons.loadFont().then();
 
   return (
     <Card>
       <Text style={{backgroundColor: 'powderblue'}}>{name}</Text>
       <Card.Cover source={{uri: image}} style={{height: 300}} />
       <Text>{address}</Text>
+      {/* <FontAwesome5 name={'paw'} /> */}
+      <View style={{flexDirection: 'row'}}>
+        {ratingArray.map((_, i) => (
+          <Ionicons
+            name={'paw'}
+            style={{color: 'orange', fontSize: 20, padding: 1}}
+          />
+        ))}
+      </View>
     </Card>
   );
 };
