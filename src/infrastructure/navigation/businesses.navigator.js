7e689ed934b3284d9764072c7fd0c59314c144bb
Cole Fortner <colefortner@gmail.com>
2022-04-12 22:15:46 -0400

    Add navigation to a map screen to businesses.navigator

--- src/infrastructure/navigation/businesses.navigator.js
+++ src/infrastructure/navigation/businesses.navigator.js
@@ -5,7 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {BusinessScreen} from '../../components/features/businesses/screens/business.screen';
 import {BusinessDetailScreen} from '../../components/features/businesses/screens/business-detail.screen';
 import {BusinessHours} from '../../components/features/businesses/screens/business-hours.screen';
-
+import {BusinessMapScreen} from '../../components/features/businesses/screens/business-map.screen';
 const BusinessStack = createNativeStackNavigator();
 
 export const BusinessesNavigator = () => {
@@ -17,6 +17,10 @@ export const BusinessesNavigator = () => {
         component={BusinessDetailScreen}
       />
       <BusinessStack.Screen name="BusinessHours" component={BusinessHours} />
+      <BusinessStack.Screen
+        name="BusinessMapScreen"
+        component={BusinessMapScreen}
+      />
     </BusinessStack.Navigator>
   );
 };
